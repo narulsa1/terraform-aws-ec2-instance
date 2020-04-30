530cfda0ae24d1b792c46ae7158ae35274c08b71
narulsa1 <sahil.narula04@gmail.com>
2020-05-01 01:23:05 +0530

    update region

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
+  region     = "ap-south-1"
 }
 
 resource "aws_instance" "myec2" {
-  ami = "ami-0d8f6eb4f641ef691"
+  ami = "ami-0470e33cd681b2476"
   instance_type = "t2.micro"
 }
